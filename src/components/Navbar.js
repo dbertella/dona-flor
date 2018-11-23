381a86d66ddab0a0f6d68fbf69f83440d2c75632
dbertella <daniele.bertella@gmail.com>
2018-11-23 23:25:33 +0000

    ⚡️ Playing with react hooks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import { setConfig } from 'react-hot-loader'
 
-const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-brand">
-        <Link to="/" className="navbar-item">
-          <div className="has-text-weight-bold is-size-2">Dōna Flor</div>
-        </Link>
-      </div>
-      <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <iframe
-          src="https://open.spotify.com/embed/album/7sgfM3mY2L8NzyGI8iqiDG"
-          width="300"
-          height="80"
-          frameborder="0"
-          allowtransparency="true"
-          allow="encrypted-media"
-        />
-      </div>
-    </div>
-  </nav>
-)
+setConfig({ pureSFC: true })
+
+const Navbar = () => {
+  const [height, setHeight] = useState(80)
+  const isCollapsed = height === 80
+  const setIframeHeight = () => (isCollapsed ? setHeight(400) : setHeight(80))
+  return (
+    <nav className="navbar is-transparent">
+        <div className="navbar-brand">
+          <Link to="/" className="navbar-item">
+            <div className="has-text-weight-bold is-size-2">Dōna Flor</div>
+          </Link>
+        </div>
+        <div className="navbar-start">
+          <Link className="navbar-item" to="/about">
+            About
+          </Link>
+        </div>
+        <div className="navbar-end">
+          <iframe
+            src="https://open.spotify.com/embed/album/7sgfM3mY2L8NzyGI8iqiDG"
+            width="300"
+            height={height}
+            frameBorder="0"
+            allowtransparency="true"
+            allow="encrypted-media"
+          />
+          <button onClick={setIframeHeight}>{isCollapsed ? 'More' : 'Hide'}</button>
+        </div>
+    </nav>
+  )
+}
 
 export default Navbar
