2397a75309a2b702b23ea257b90aa76a76ac7545
dbertella <daniele.bertella@gmail.com>
2018-11-24 15:06:07 +0000

    Update cms to add home page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,11 @@
 import CMS from 'netlify-cms'
 
+import HomePagePreview from './preview-templates/HomePagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 
+CMS.registerPreviewTemplate('home', HomePagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
