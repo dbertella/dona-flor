bf1c3afab2b9b796068f233d9f2b5a107ce8bade
dbertella <daniele.bertella@gmail.com>
2018-11-24 15:08:10 +0000

    Fix typo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Instagram } from './instagram'
+import { Instagram } from './Instagram'
 import { Facebook } from './Facebook'
 
 const Navbar = () => {
