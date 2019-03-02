41a9022bd0c379b9eaf08ee0d279b20f28ddfd08
dbertella <daniele.bertella@gmail.com>
2019-03-02 18:39:33 +0000

    Use gatsby image

--- src/components/FullWidthImg.tsx
+++ src/components/FullWidthImg.tsx
@@ -1,22 +1,18 @@
 import React from 'react'
 import styled from 'styled-components'
+import Img, { GatsbyImageProps } from 'gatsby-image'
 
-export const FullWidthImg = styled.div`
+export const FullWidthImg = styled(Img)`
   height: 400px;
-  background-size: cover;
-  background-position: center center;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   margin-top: 0;
 `
 
-export default ({ image }) => (
-  <FullWidthImg
-    style={{
-      backgroundImage: `url(${
-        !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-      })`
-    }}
-  />
-)
+export default ({ image }: { image: { childImageSharp: GatsbyImageProps } }) => {
+  if (!image) {
+    return null
+  }
+  if (typeof image === 'string') {
+    return <img src={image} alt="" />
+  }
+  return <FullWidthImg fluid={image.childImageSharp.fluid} alt="" />
+}
