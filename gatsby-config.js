07dd266ca348e6c3475d54e896a1ec1c9f0cbf1f
Daniele Bertella <daniele.bertella@gmail.com>
2020-06-07 12:57:13 +0100

    Bump instagram scraping plugin to beta version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'Dōna Flor',
+    title: "Dōna Flor",
     description:
-      '💃🏻Dōna Flor è una band composta da 5 musicisti (voce, fisarmonica, chitarra, contrabbasso, batteria/percussioni), che propone un repertorio dal sapore prevalentemente sudamericano e gitano, una "free etnomusic" fatta di brani tradizionali e originali, cumbie, sonorità capoverdiane, ma anche tradizionali balcanici e klezmer, con arrangiamenti originali e contaminazioni jazz.'
+      '💃🏻Dōna Flor è una band composta da 5 musicisti (voce, fisarmonica, chitarra, contrabbasso, batteria/percussioni), che propone un repertorio dal sapore prevalentemente sudamericano e gitano, una "free etnomusic" fatta di brani tradizionali e originali, cumbie, sonorità capoverdiane, ma anche tradizionali balcanici e klezmer, con arrangiamenti originali e contaminazioni jazz.',
   },
   plugins: [
     {
-      resolve: 'gatsby-plugin-layout',
+      resolve: "gatsby-plugin-layout",
       options: {
-        component: require.resolve('./src/components/Layout.tsx')
-      }
+        component: require.resolve("./src/components/Layout.tsx"),
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
@@ -20,110 +20,110 @@ module.exports = {
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
-        respectDNT: true
-      }
+        respectDNT: true,
+      },
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-typescript',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-sass",
     {
       resolve: `gatsby-source-instagram`,
       options: {
-        username: `donaflor.music`,
+        username: `6773082462`, // `donaflor.music`,
       },
     },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Dōna Flor',
-        short_name: 'Dōna Flor',
-        start_url: '/',
-        background_color: '#6b37bf',
-        theme_color: '#6b37bf',
+        name: "Dōna Flor",
+        short_name: "Dōna Flor",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
         icons: [
           {
-            src: 'static/img/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "static/img/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: 'static/img/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
+            src: "static/img/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
         ],
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
-        display: 'standalone',
-        icon: 'static/img/favicon-32x32.png' // This path is relative to the root of the site.
-      }
+        display: "standalone",
+        icon: "static/img/favicon-32x32.png", // This path is relative to the root of the site.
+      },
     },
     {
       // keep as first gatsby-source-filesystem plugin for gatsby image support
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/static/img`,
-        name: 'uploads'
-      }
+        name: "uploads",
+      },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages'
-      }
+        name: "pages",
+      },
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images'
-      }
+        name: "images",
+      },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-relative-images',
+            resolve: "gatsby-remark-relative-images",
             options: {
-              name: 'uploads'
-            }
+              name: "uploads",
+            },
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               // It's important to specify the maxWidth (in pixels) of
               // the content container as this plugin uses this as the
               // base for generating different widths of each image.
-              maxWidth: 2048
-            }
+              maxWidth: 2048,
+            },
           },
           {
-            resolve: 'gatsby-remark-copy-linked-files',
+            resolve: "gatsby-remark-copy-linked-files",
             options: {
-              destinationDir: 'static'
-            }
-          }
-        ]
-      }
+              destinationDir: "static",
+            },
+          },
+        ],
+      },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`
-      }
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
     },
     {
-      resolve: 'gatsby-plugin-purgecss', // purges all unused/unreferenced css rules
+      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
       options: {
         develop: true, // Activates purging in npm run develop
-        purgeOnly: ['/all.sass'] // applies purging only on the bulma css file
-      }
+        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
+      },
     }, // must be after other CSS plugins
-    'gatsby-plugin-netlify' // make sure to keep it last in the array
-  ]
-}
+    "gatsby-plugin-netlify", // make sure to keep it last in the array
+  ],
+};
