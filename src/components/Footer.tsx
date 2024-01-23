804d7820e830e5b88cb3a29700eae207d29b4c20
Daniele <daniele.bertella@gmail.com>
2024-01-23 22:00:01 +0000

    update spotify album

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,30 @@
-import React, { useState } from 'react'
-import { setConfig } from 'react-hot-loader'
+import React, { useState } from "react";
+import { setConfig } from "react-hot-loader";
 
-setConfig({ pureSFC: true })
+setConfig({ pureSFC: true });
 
 const Footer = () => {
-  const [height, setHeight] = useState(80)
-  const isCollapsed = height === 80
-  const setIframeHeight = () => (isCollapsed ? setHeight(400) : setHeight(80))
+  const [height, setHeight] = useState(80);
+  const isCollapsed = height === 80;
+  const setIframeHeight = () => (isCollapsed ? setHeight(400) : setHeight(80));
   return (
     <footer className="is-flex" style={{ height }}>
       <iframe
-        src="https://open.spotify.com/embed/album/7sgfM3mY2L8NzyGI8iqiDG"
+        src="https://open.spotify.com/embed/album/1wk3KaZ3DZK8txcY8ZCSJ5"
         width="100%"
         height={height}
         frameBorder="0"
-        allow="encrypted-media"
-        title="Alma Desnuda - Dōna Flor"
-      />
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
       <button
         className="has-text-dark has-background-black button-playlist"
         onClick={setIframeHeight}
       >
-        {isCollapsed ? 'More' : 'Hide'}
+        {isCollapsed ? "More" : "Hide"}
       </button>
     </footer>
-  )
-}
-export default Footer
+  );
+};
+export default Footer;
